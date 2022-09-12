0d08fb03b086e6007dc42539c78513645d09089c
Leon Chu <Ljchu87@gmail.com>
2022-09-12 17:49:32 -0500

    Add start button

--- js/app.js
+++ js/app.js
@@ -10,17 +10,20 @@ let board, turn, winner
 
 const circleEls = document.querySelector(".grid")
 const boardEls = document.querySelectorAll(".grid > div")
+const messageEl = document.querySelector("#game-status")
+const startBtn = document.querySelector("#start-button")
 
 
 /*----------------------- Event Listeners -----------------------------*/
 
 circleEls.addEventListener("click", handleClick)
+startBtn.addEventListener("click", init)
 
 /*-------------------------- Functions --------------------------------*/
 
 init()
 function init() {
-  boardArr = [
+  board = [
     null, null, null, null, null, null, null, 
     null, null, null, null, null, null, null, 
     null, null, null, null, null, null, null, 
@@ -33,8 +36,32 @@ function init() {
   render()
 }
 
+function render() {
+  board.forEach(function(circle, idx){
+    circleEls[idx].innerText = circle;
+    if (circle === 1) {
+      circleEls[idx].textContent = "X"
+    }
+    else if (circle === -1) {
+      circleEls[idx].textContent= "O"
+    }
+    else {
+      circleEls[idx].innerText = ""
+    }
+  })
+
+  if (!winner){
+    messageEl.textContent = "It's a tie!"
+  } else if (winner === 1) {
+    messageEl.textContent = "CONGRATS! Player 1 wins the game!"
+  } else if (winner === -1) {
+    messageEl.textContent = "CONGRATS! Player 2 wins the gane!"
+  }
+}
+
+
 function handleClick(evt){
   let idx = parseInt(evt.target.id.replace("token", ""))
   console.log("idx that was clicked", idx);
-}
 
+}
